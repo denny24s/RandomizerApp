d6aa558d67361566437bb73e7c6a8359f35b31cd
denysshumik <shumikdenys@gmail.com>
2025-05-09 22:36:25 +0300

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,13 @@ dependencies {
     implementation(project(":domain"))
     implementation(project(":data"))
 
+    // ViewModel & Lifecycle
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")  // ViewModel coroutine support
+    // Compose <–> ViewModel integration
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
